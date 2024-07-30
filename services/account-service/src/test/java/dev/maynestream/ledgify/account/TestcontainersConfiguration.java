0074b7cf0052d0e024393d077368d6274eed184e
maynestream-dev <chris@maynestream.dev>
2024-07-30 03:43:23 +0100

    Update configuration

--- services/account-service/src/test/java/dev/maynestream/ledgify/account/TestcontainersConfiguration.java
+++ services/account-service/src/test/java/dev/maynestream/ledgify/account/TestcontainersConfiguration.java
@@ -4,8 +4,10 @@ import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 
+@TestPropertySource(properties = "grpc.server.port=0")
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
